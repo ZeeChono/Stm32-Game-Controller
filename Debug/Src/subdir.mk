9ee209829c751ea838feff6ab5ff08ea146296ce
Yi Wei <59746876+jyiwei@users.noreply.github.com>
2021-11-19 16:54:34 -0500

    implemented key long-press

---
+++ Debug/Src/subdir.mk
@@ -0,0 +1,44 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/stm32l4xx_hal_msp.c \
+../Src/stm32l4xx_it.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/system_stm32l4xx.c \
+../Src/usb_device.c \
+../Src/usbd_conf.c \
+../Src/usbd_desc.c 
+
+OBJS += \
+./Src/main.o \
+./Src/stm32l4xx_hal_msp.o \
+./Src/stm32l4xx_it.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/system_stm32l4xx.o \
+./Src/usb_device.o \
+./Src/usbd_conf.o \
+./Src/usbd_desc.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/stm32l4xx_hal_msp.d \
+./Src/stm32l4xx_it.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/system_stm32l4xx.d \
+./Src/usb_device.d \
+./Src/usbd_conf.d \
+./Src/usbd_desc.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L475xx -c -I../Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
